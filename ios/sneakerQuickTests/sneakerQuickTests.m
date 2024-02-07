7c9cfb12464d63fb6d12ed3b600088d8821f890f
Isaque Akamine <isaqueakamine@gmail.com>
2024-02-06 21:12:31 -0300

    feat: change app name and bundle package

--- ios/sneakerQuickTests/sneakerQuickTests.m
+++ ios/sneakerQuickTests/sneakerQuickTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface sneakerQuickTests : XCTestCase
+@interface SneakerQuickTests : XCTestCase
 
 @end
 
-@implementation sneakerQuickTests
+@implementation SneakerQuickTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
